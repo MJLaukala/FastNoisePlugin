0e577cde64afa308acc2bc4ee27ec4cf16d14d6b
MJLaukala <mjlaukala@gmail.com>
2021-07-17 03:49:23 -0700

    Added noise by blueprint graph

---
+++ Source/FastNoisePlugin/Private/FastNoiseGraphFunctionLibrary.cpp
@@ -0,0 +1,70 @@
+// Copyright 2021 Matthew Laukala
+
+
+#include "FastNoiseGraphFunctionLibrary.h"
+
+FNoiseOperation2D::FNoiseOperation2D() : DefaultValue(0)
+{
+	OperationMathLambda = [=](const float X, const float Y)
+	{
+		return DefaultValue;
+	};
+}
+
+FNoiseOperation2D::FNoiseOperation2D(float InDefaultValue) : DefaultValue(InDefaultValue)
+{
+	OperationMathLambda = [=](const float X, const float Y)
+	{
+		return DefaultValue;
+	};
+}
+
+float FNoiseOperation2D::GetValue(float X, float Y) const
+{
+	return OperationMathLambda(X, Y);
+}
+
+FNoiseOperation2D UFastNoiseGraphFunctionLibrary::Add_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B)
+{
+	FNoiseOperation2D Operation;
+	Operation.OperationMathLambda = [=](const float X, const float Y)
+	{
+		return A.OperationMathLambda(X, Y) + B.OperationMathLambda(X, Y);
+	};
+	return Operation;
+}
+
+FNoiseOperation2D UFastNoiseGraphFunctionLibrary::Subtract_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B)
+{
+	FNoiseOperation2D Operation;
+	Operation.OperationMathLambda = [=](const float X, const float Y)
+	{
+		return A.OperationMathLambda(X, Y) - B.OperationMathLambda(X, Y);
+	};
+	return Operation;
+}
+
+FNoiseOperation2D UFastNoiseGraphFunctionLibrary::Multiply_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B)
+{
+	FNoiseOperation2D Operation;
+	Operation.OperationMathLambda = [=](const float X, const float Y)
+	{
+		return A.OperationMathLambda(X, Y) * B.OperationMathLambda(X, Y);
+	};
+	return Operation;
+}
+
+FNoiseOperation2D UFastNoiseGraphFunctionLibrary::Divide_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B)
+{
+	FNoiseOperation2D Operation;
+	Operation.OperationMathLambda = [=](const float X, const float Y)
+	{
+		return A.OperationMathLambda(X, Y) / B.OperationMathLambda(X, Y);
+	};
+	return Operation;
+}
+
+float UFastNoiseGraphFunctionLibrary::CalculateNoiseOperation2D(const FNoiseOperation2D& ValueOperation2D, float X, float Y)
+{
+	return ValueOperation2D.GetValue(X, Y);
+}
---
+++ Source/FastNoisePlugin/Public/FastNoiseGraphFunctionLibrary.h
@@ -0,0 +1,349 @@
+// Copyright 2021 Matthew Laukala
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include <functional>
+
+#include "FastNoiseFunctionLibrary.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FastNoiseGraphFunctionLibrary.generated.h"
+
+
+typedef std::function<float(float X, float Y)> F2DOperationLambda; 
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float DefaultValue;
+	
+	F2DOperationLambda OperationMathLambda;
+
+	GENERATED_BODY()
+
+		FNoiseOperation2D();
+	FNoiseOperation2D(float InDefaultValue);
+	
+	virtual ~FNoiseOperation2D() = default;
+
+		float GetValue(float X, float Y) const;
+};
+
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FValue2DOperation : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FNoiseOperation2D Frequency;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	EInterpolation Interpolation;
+
+	GENERATED_BODY()
+
+	FValue2DOperation() : Seed(0), Frequency(0), Interpolation(EInterpolation::Linear)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetValue2D(Seed, Frequency.GetValue(X, Y), Interpolation, X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FValueFractal2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Frequency;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Lacunarity;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Gain;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Octaves;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		EInterpolation Interpolation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		EFractalType FractalType;
+
+	GENERATED_BODY()
+
+		FValueFractal2D() : Seed(0), Octaves(0), Interpolation(EInterpolation::Linear), FractalType(EFractalType::Billow)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetValueFractal2D(Seed, Frequency.GetValue(X, Y), Lacunarity.GetValue(X, Y), Gain.GetValue(X, Y), Octaves, Interpolation, FractalType, X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FPerlin2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Frequency;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Lacunarity;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		EInterpolation Interpolation;
+
+	GENERATED_BODY()
+
+		FPerlin2D() : Seed(0), Interpolation(EInterpolation::Linear)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetPerlin2D(Seed, Frequency.GetValue(X, Y), Interpolation, X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FPerlinFractal2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Frequency;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Lacunarity;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Gain;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Octaves;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		EInterpolation Interpolation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		EFractalType FractalType;
+
+	GENERATED_BODY()
+
+		FPerlinFractal2D() : Seed(0), Octaves(0), Interpolation(EInterpolation::Linear), FractalType(EFractalType::Billow)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetPerlinFractal2D(Seed, Frequency.GetValue(X, Y), Lacunarity.GetValue(X, Y), Gain.GetValue(X, Y), Octaves, Interpolation, FractalType, X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FSimplex2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Frequency;
+
+	GENERATED_BODY()
+
+		FSimplex2D() : Seed(0)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetSimplex2D(Seed, Frequency.GetValue(X, Y), X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FSimplexFractal2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Frequency;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Lacunarity;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Gain;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Octaves;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		EFractalType FractalType;
+
+	GENERATED_BODY()
+
+		FSimplexFractal2D() : Seed(0), Octaves(0), FractalType(EFractalType::Billow)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetSimplexFractal2D(Seed, Frequency.GetValue(X, Y), Lacunarity.GetValue(X, Y), Gain.GetValue(X, Y), Octaves, FractalType, X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FWhiteNoise2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	GENERATED_BODY()
+
+		FWhiteNoise2D() : Seed(0)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetWhiteNoise2D(Seed, X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FWhiteNoiseInt2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	GENERATED_BODY()
+
+		FWhiteNoiseInt2D() : Seed(0)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetWhiteNoiseInt2D(Seed, X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FCubic2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Frequency;
+
+	GENERATED_BODY()
+
+		FCubic2D() : Seed(0)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetCubic2D(Seed, Frequency.GetValue(X, Y), X, Y);
+		};
+	}
+};
+
+USTRUCT(BlueprintType)
+struct FASTNOISEPLUGIN_API FCubicFractal2D : public FNoiseOperation2D
+{
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Seed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Frequency;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Lacunarity;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FNoiseOperation2D Gain;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		int32 Octaves;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		EFractalType FractalType;
+
+	GENERATED_BODY()
+
+		FCubicFractal2D() : Seed(0), Octaves(0), FractalType(EFractalType::Billow)
+	{
+		OperationMathLambda = [=](const float X, const float Y)
+		{
+			return UFastNoiseFunctionLibrary::GetCubicFractal2D(Seed, Frequency.GetValue(X, Y), Lacunarity.GetValue(X, Y), Gain.GetValue(X, Y), Octaves, FractalType, X, Y);
+		};
+	}
+};
+
+/**
+ * 
+ */
+UCLASS()
+class FASTNOISEPLUGIN_API UFastNoiseGraphFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "FValue2DOperation To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FValue2DOperationToFNoiseOperation2D(const FValue2DOperation& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FValueFractal2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FValueFractal2DToFValueOperation2D(const FValueFractal2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FPerlin2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FPerlin2DToFValueOperation2D(const FPerlin2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FPerlinFractal2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FPerlinFractal2DToFValueOperation2D(const FPerlinFractal2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FSimplex2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FSimplex2DToFValueOperation2D(const FSimplex2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FSimplexFractal2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FSimplexFractal2DToFValueOperation2D(const FSimplexFractal2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FWhiteNoise2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FWhiteNoise2DToFValueOperation2D(const FWhiteNoise2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FWhiteNoiseInt2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FWhiteNoiseInt2DToFValueOperation2D(const FWhiteNoiseInt2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FCubic2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FCubic2DToFValueOperation2D(const FCubic2D& Value) { return Value; }
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FCubicFractal2D To FNoiseOperation2D", CompactNodeTitle = "->", ScriptMethod = "FNoiseOperation2D", BlueprintAutoCast), Category = "FastNoise")
+		static FNoiseOperation2D Conv_FCubicFractal2DToFValueOperation2D(const FCubicFractal2D& Value) { return Value; }
+		
+#undef CONVERSION
+	
+	// MATHS
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "FNoiseOperation2D + FNoiseOperation2D", CompactNodeTitle = "+", Keywords = "+ add plus", CommutativeAssociativeBinaryOperator = "true"), Category = "Math|FastNoise")
+	static FNoiseOperation2D Add_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B);
+
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "FNoiseOperation2D - FNoiseOperation2D", CompactNodeTitle = "-", Keywords = "- subtract minus", CommutativeAssociativeBinaryOperator = "true"), Category = "Math|FastNoise")
+	static FNoiseOperation2D Subtract_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B);
+
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "FNoiseOperation2D * FNoiseOperation2D", CompactNodeTitle = "*", Keywords = "* multiply times", CommutativeAssociativeBinaryOperator = "true"), Category = "Math|FastNoise")
+	static FNoiseOperation2D Multiply_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B);
+
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "FNoiseOperation2D / FNoiseOperation2D", CompactNodeTitle = "/", Keywords = "/ divide divided", CommutativeAssociativeBinaryOperator = "true"), Category = "Math|FastNoise")
+	static FNoiseOperation2D Divide_FNoiseOperation2DFNoiseOperation2D(const FNoiseOperation2D& A, const FNoiseOperation2D& B);
+
+	UFUNCTION(BlueprintPure, Category = "FastNoise", meta = (DisplayName = "GetValue"))
+	static float CalculateNoiseOperation2D(const FNoiseOperation2D& ValueOperation2D, float X, float Y);
+
+};
